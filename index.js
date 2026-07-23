b04ee9091fe97561f80ab67d2ea1d69d6b324699
Pinon1345 <pinonfurkan1@gmail.com>
2026-07-24 02:46:38 +0600

    intetrage payment method with stripe

--- index.js
+++ index.js
@@ -34,12 +34,29 @@ async function run() {
 
 
         const database = client.db("hireloop_db");
+        const userCollection = database.collection("user");
         const jobCollection = database.collection("jobs");
         const companyCollection = database.collection("companies");
+        const applicationsCollection = database.collection("applications");
+        const planCollection = database.collection("plans");
+        const subscriptionCollection = database.collection("subscriptions");
 
 
 
         // API Start
+
+
+        // User get related API
+
+        app.get("/api/users", async (req, res) => {
+            const email = req.query.email;
+
+            const user = await userCollection.findOne({
+                email: email,
+            });
+
+            res.send(user || {});
+        });
 
 
         // Post API for new job
@@ -82,6 +99,36 @@ async function run() {
             res.send(result);
 
 
+        })
+
+        // Application related API
+
+        // Get API for job application
+
+        app.get('/api/applications', async (req, res) => {
+            const query = {}
+            if (req.query.applicantId) {
+                query.applicantId = req.query.applicantId;
+            }
+            if (req.query.jobId) {
+                query.jobId = req.query.jobId;
+            }
+            const cursor = applicationsCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result);
+        })
+
+
+        // Post API for new job application
+
+        app.post('/api/applications', async (req, res) => {
+            const application = req.body
+            const newApplication = {
+                ...application,
+                createdAt: new Date()
+            }
+            const result = await applicationsCollection.insertOne(newApplication)
+            res.send(result);
         })
 
 
@@ -129,6 +176,45 @@ async function run() {
             }
             const result = await companyCollection.findOne(query)
             res.send(result || {});
+        })
+
+        // AIP for Plans
+
+        // get API
+
+        app.get('/api/plans', async (req, res) => {
+            const query = {}
+            if (req.query.plan_id) {
+                query.id = req.query.plan_id
+            }
+            const plan = await planCollection.findOne(query)
+            res.send(plan);
+        })
+
+        // API for Subscriptions
+
+        // post API
+
+        app.post('/api/subscriptions', async (req, res) => {
+            const data = req.body
+            const subscriptionInfo = {
+                ...data,
+                createdAt: new Date()
+            }
+
+            const result = await subscriptionCollection.insertOne(subscriptionInfo)
+
+            // Update the user plan information
+
+            const filter = { email: data.email };
+            const updateDocument = {
+                $set: {
+                    plan: data.planId,
+                },
+            }
+
+            const updateResult = await userCollection.updateOne(filter, updateDocument)
+            res.send(updateResult);
         })
 
 
